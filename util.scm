fae90eb74164d1b2355471e5c10cc93c1261de3d
Yaolong Huang <airekans@gmail.com>
2012-10-28 01:06:16 -0700

    Begin to write the read procedure.

--- util.scm
+++ util.scm
@@ -18,3 +18,34 @@
                      (procedure-body object)
                      '<procedure-env>))
       (display object)))
+
+(define (make-line-col-index line col)
+  (cons line col))
+(define (line-index lci) (car lci))
+(define (col-index lci) (cdr lci))
+(define (line-index-1+ lci) (cons (1+ (car lci)) (cdr lci)))
+(define (col-index-1+ lci) (cons (car lci) (1+ (cdr lci))))
+
+(define (make-indexed-token tok beg-cli end-cli)
+  (list tok beg-cli end-cli))
+(define (token itok) (car itok))
+(define (beg-cli itok) (cadr itok))
+(define (end-cli itok) (caddr itok))
+
+(define delimiters (string->char-set " ()"))
+
+(define (read-sexp)
+  (let ((buffer "")
+	(line-num 0)
+	(col-num 0))
+    (read-line (string->char-set "() "))))
+
+(define (read-new-self-eval beg)
+  (define (read-self-eval buffer end)
+    (if (char-set-member? delimiters (peek-char))
+	(make-indexed-token (read (open-input-string
+				   (reverse-string (list->string buffer))))
+			    beg end)
+	(read-self-eval (cons (read-char) buffer) (col-index-1+ end))))
+  (read-self-eval '() beg))
+
